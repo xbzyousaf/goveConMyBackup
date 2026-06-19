2fbb3bce8449121bb290bbae0745d53eb6aefe9d
DELL-8th-gen\DELL <xb.zyousaf@gmail.com>
2026-06-19 12:29:07 +0500

    removed jest, and vednor onboarding bug

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,22 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
+// export default {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
 
-  testMatch: ["**/server/**/*.test.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/client/"],
+//   testMatch: ["**/server/**/*.test.ts"],
+//   testPathIgnorePatterns: ["/node_modules/", "/client/"],
 
-  moduleNameMapper: {
-    "^@shared/(.*)$": "<rootDir>/shared/$1",
-    "^server/(.*)$": "<rootDir>/server/$1",
-    "^constants/(.*)$": "<rootDir>/constants/$1",
-  },
+//   moduleNameMapper: {
+//     "^@shared/(.*)$": "<rootDir>/shared/$1",
+//     "^server/(.*)$": "<rootDir>/server/$1",
+//     "^constants/(.*)$": "<rootDir>/constants/$1",
+//   },
 
-  setupFiles: ["dotenv/config"],
+//   setupFiles: ["dotenv/config"],
 
-  transform: {
-    "^.+\\.ts$": ["ts-jest", { diagnostics: false }],
-  },
-   // 🔥 ADD THIS LINE
-  clearMocks: true,
-  resetModules: true,
-};
+//   transform: {
+//     "^.+\\.ts$": ["ts-jest", { diagnostics: false }],
+//   },
+//    // 🔥 ADD THIS LINE
+//   clearMocks: true,
+//   resetModules: true,
+// };
